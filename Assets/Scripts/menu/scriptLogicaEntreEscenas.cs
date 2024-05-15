a9bde0c567415ff7a1579ef79cc1df3aca5fa2ef
Marco Garcia <garciacorreamarco@gmail.com>
2024-05-14 23:21:20 -0500

    paso entre escenas

---
+++ Assets/Scripts/menu/scriptLogicaEntreEscenas.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptLogicaEntreEscenas : MonoBehaviour
+{
+    private void Awake()
+    {
+        var DontDestroyScenes = FindObjectsOfType<scriptLogicaEntreEscenas>();
+        if (DontDestroyScenes.Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
